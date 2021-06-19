6d6a6d61334ddd0cefdf9c3ca324bfc10953cd92
y3sar <saminc97@gmail.com>
2021-06-19 18:37:26 +0600

    pass_gen always outputs valid password

--- src/pass_gen.py
+++ src/pass_gen.py
@@ -1,27 +1,92 @@
 import base64
 import hashlib
 import os
+import random
+
+
+NO_NUMBER = 'n'
+NO_LOWER = 'l'
+NO_UPPER = 'u'
+
+
 
 def gen_password(website_name, master_pass, private_key=''):
   if private_key == '':
+    postfix = str(random.randint(100, 999));
     # generate random private_key
-    private_key = hashlib.sha256(os.urandom(128)).hexdigest();
+    private_key = hashlib.sha256(os.urandom(128)).hexdigest() + postfix;
+  else:
+    postfix = private_key[-3:]
 
-  joined_text = (private_key+website_name+master_pass).encode('utf-8');
+  joined_text = (website_name+private_key+master_pass).encode('utf-8');
 
   password_hash = hashlib.sha256(joined_text).hexdigest();
-  print(password_hash);
 
   # to add capital letters 
-  password = base64.b64encode(password_hash.encode('utf-8'));
+  password = base64.b64encode(password_hash.encode('utf-8')).decode()[:12];
+  
+  is_valid = is_pass_valid(password);
 
-  return password.decode()[:12], private_key;
+  if is_valid == '':
+    return password, private_key;
+
+  print(is_valid);
+  print(password + ' -> ' + fix_password(password, is_valid, postfix));
+
+  return fix_password(password, is_valid, postfix), private_key;
 
 
-new_password = gen_password('twitch', 'bohogarden');
+def fix_password(password, token, postfix):
+  ret = list(password);
 
+
+  if 'n' in token:
+    n_idx = int(str(postfix)[0]);
+    new_char = str(ord(ret[n_idx]))[-1];
+    ret[n_idx] = new_char;
+
+  if 'l' in token:
+    l_idx = int(str(postfix)[1]);
+    new_char = ret[l_idx].lower();
+    ret[l_idx] = new_char;
+
+  if 'u' in token:
+    u_idx = int(str(postfix)[2]);
+    new_char = ret[u_idx].upper();
+    ret[u_idx] = new_char;
+
+  return ''.join(ret);
+
+
+
+def is_pass_valid(password):
+  """Checks if a password is valid (has atleast one number, lowercase, uppercase"""
+  ret = "nlu"
+
+  for char in password:
+    if 'l' in ret and char.islower():
+      ret = ret.replace("l", "");
+
+    if 'u' in ret and char.isupper():
+      ret = ret.replace("u", "");
+
+    if 'n' in ret and char.isnumeric():
+      ret = ret.replace("n", "");
+
+  return ret;
+
+
+
+
+new_password, private_key = gen_password('twitch', 'bohogarden');
+new_password2, _ = gen_password('twitter', 'bohogarden', private_key=private_key);
+new_password3, _ = gen_password('facebook', 'bohogarden', private_key=private_key);
 
 print(new_password);
+print(new_password2);
+print(new_password3);
+
+
 
 
 
